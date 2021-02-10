6ea4b1920b7c696104114d04579c8e64459e933a
SmashScharrer <SmashScharrer-Dev@outlook.fr>
2021-02-10 15:38:02 +0100

    Update Webpack-encore & Add VueJS & SASS

---
+++ assets/vue/HomeSecond.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-second-page">
+    <h1 class="h1">Bonjour</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSecondPage"
+}
+</script>
+
+<style scoped>
+
+</style>
